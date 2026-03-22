a95475958a46e359d57dfa7c6030955406f18498
raoxikui <raoxikui@qq.com>
2026-03-22 22:31:40 +0800

    优化

---
+++ src/Models/SystemMessageLog.php
@@ -0,0 +1,11 @@
+<?php
+/*
+    |__________________________________________________
+    |  DagaSmart CE pro by 3.0.17  |
+    |  Authorization on 2026-03-22 00:00:29              |
+    |  GitHub: https://github.com/dagasmart/bizadmin    |
+    |  Tel: 13511953492   |
+    |  Email: dagasmart@qq.com   |
+    |__________________________________________________|
+*/
+ namespace DagaSmart\BizAdmin\Models; use Illuminate\Database\Eloquent\Relations\HasOne; use Illuminate\Database\Eloquent\SoftDeletes; use DagaSmart\BizAdmin\Models\BaseModel as Model; class SystemMessageLog extends Model { protected $table = "\163\x79\163\x74\145\155\137\155\145\163\x73\141\x67\x65\137\154\157\x67"; protected $primaryKey = "\151\144"; }
